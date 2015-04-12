94cec2ed6837a141764bb5b54e98a0f86c843cd7
Debanjan Dasgupta <DebanjansDasgupta@Gmail.com>
2015-04-12 11:50:40 -0400

    added object wrapper to fetch

--- api/routes/wander_list.rb
+++ api/routes/wander_list.rb
@@ -14,12 +14,24 @@ module Sinatra
             parkId = bparams['parkId']
             
             if userId != nil && parkId != nil then
-              UserList.new({:user_id => userId, :park_id => parkId}).insert
+              UserList.find_or_create_by(:user_id => userId, :park_id => parkId)
               
-              # parks = UserList.where(:user_id => userId).(:park_id)
-              # UserList.where(:user_id.ne => userId).distinct(:user_id)
-#               
-              # puts UserList.not.where(:park_id.in => parks).and.where(:user_id.ne => userId).to_a.to_json
+              # #reccomendation prototype
+              # Recommendation.where(:user_id => userId, :park_id => parkId).delete
+              # parks = UserList.where(:user_id => userId).distinct(:park_id)
+              # # UserList.where(:user_id.ne => userId).distinct(:user_id)
+              # phash = {}
+              # phash.default = 0
+              # similar =  UserList.only(:park_id).not.where(:park_id.in => parks).and.where(:user_id.ne => userId).to_a
+              # similar.each do |s|
+                # phash[s['parkId']] += 1
+              # end
+              # phash.each do |k,v|
+                # r = Reccomendation.find_or_create_by(:user_id => userId, :park_id => k)
+                # r[priority] += v
+                # r.update
+              # end
+              
               return '{}'
             else
               return '{failure}'
@@ -41,7 +53,7 @@ module Sinatra
           
           fetch_list = lambda do
             userId = params['userId']
-            return UserList.only(:user_id, :park_id).where(:user_id => userId).to_a.to_json
+            return  {"results" => UserList.only(:user_id, :park_id).where(:user_id => userId).to_a}.to_json
           end
           
           fetch_all = lambda do
