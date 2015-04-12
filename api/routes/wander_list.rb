8be865d6f8cde7486c8eea0cad57753070d8b61d
Debanjan Dasgupta <DebanjansDasgupta@Gmail.com>
2015-04-12 10:24:28 -0400

    adding fetch all

--- api/routes/wander_list.rb
+++ api/routes/wander_list.rb
@@ -1,4 +1,5 @@
 require_relative '../models/user_list'
+require_relative '../models/recommendation'
 
 module Sinatra
   module SampleApp
@@ -38,10 +39,16 @@ module Sinatra
             return UserList.where(:user_id => userId).to_a.to_json
           end
           
+          fetch_all = lambda do
+            userId = params['userId']
+            return UserList.all.to_a.to_json
+          end
+          
           
           app.post '/userList/add', &add_list
           app.post '/userList/remove', &remove_list
           app.get '/userList/fetch/:userId', &fetch_list
+          app.get '/userList/fetchAll', &fetch_all
 
         end
         
