b7e39517b5d99075b7adfd7b1e1f0d7cb8222321
Michael Bray <bray_michael@bah.com>
2015-04-12 16:03:10 -0400

    Updated GitHub Repo name, added map to List screen

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -24,6 +24,7 @@ angular.module('myAmericaApp')
     FetchUserList.get({"userId": $rootScope.email}, function (results) {
       console.log(results);
       results.results.forEach(getParkData);
+		$scope.createMap(results);
     });
 
     //$rootScope.temp.forEach(getParkData);
@@ -33,11 +34,53 @@ angular.module('myAmericaApp')
       dbParkCall(parkId.parkId);
     }
 
+	$scope.createMap = function(results) {
+		var map = L.map('mapResults').setView([$scope.lat, $scope.lng], 10);
+
+		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
+
+		$(results.results).each(function(i, v){
+			console.log(v);
+			if (v.RecAreaLatitude != "" && v.RecAreaLongitude != ""){
+				var title = v.RecAreaName;
+				var contactEmail = v.RecAreaEmail;
+				var contactPhone = v.RecAreaPhone;
+				var website = v.RecAreaMapURL;
+
+				var popUpText = "<strong>" + title + "</strong>";
+
+				if (website != ""){
+					popUpText = popUpText + "<p><a href='"+website+"'>" + website + "</a></p>";
+				}
+
+				if (contactEmail != ""){
+					popUpText = popUpText + "<p>Email Address: <a href='mailto:"+contactEmail+"'>" + contactEmail + "</a></p>";
+				}
+
+				if (contactPhone != ""){
+					popUpText = popUpText + "<p>" + contactPhone + "</p>";
+				}
+
+				v.photoQuery = ("SELECT  metadata FROM 1zi67I9StNeOzf5qv-wQ6WfR3n0ok_hDm6fSy0kI1 WHERE ST_INTERSECTS(geometry, CIRCLE(LATLNG("+v.RecAreaLatitude+","+ v.RecAreaLongitude+"), 50000))");
+				Flickr.get({key:"AIzaSyAY3kjup98kSZ5OQ4iaxFRxWqwvtLLXfPM", sql: v.photoQuery}, function(results){
+					console.log(v.photoData);
+				});
+
+				L.marker([v.RecAreaLatitude, v.RecAreaLongitude]).addTo(map)
+					.bindPopup(popUpText)
+					.openPopup();
+			}
+		});
+	};
+
     function dbParkCall(parkId) {
       RecArea.get({"apikey": RIDB_API_KEY, "parkId": parkId}, function (results) {
         console.log(results);
         $scope.userList[parkId.toString()] = results;
 
+		//$scope.createMap(results);
       });
     }
 
